3ce277e234765c8e7520f722b4cc8dcb861de6c4
Lui de la Parra <lui@mutesymphony.com>
2020-01-04 13:56:32 -0800

    Adds Firebase Utilities
    This commit adds a firebase utilities service to split the query logic into a utility service and simplify the firebase cleint.

---
+++ src/utilities/firebase.utilities.js
@@ -0,0 +1,60 @@
+const _ = require('lodash');
+const flatten = require('flat');
+
+const addWhereToRef = (ref, where) => {
+  if (!Array.isArray(where)) {
+    throw new Error('where parameter must be an array.');
+  }
+
+  if (Array.isArray(where[0])) {
+    return where.reduce((acc, whereArgs) => addWhereToRef(acc, whereArgs), ref);
+  }
+
+  return ref.where(...where);
+};
+
+const addOptionsToRef = (ref, options) => {
+  if (options.offset) {
+    ref.startAt(offset)
+  }
+
+  if (options.limit) {
+    ref.limit(options.limit)
+  }
+
+  return ref.where(...where);
+};
+
+const mapValuesDeep = (obj, fn) =>
+  _.mapValues(obj, (val, key) =>
+    _.isPlainObject(val) ? mapValuesDeep(val, fn) : fn(val, key, obj)
+  );
+
+const convertQuery = query => {
+  const queries = [];
+  const ids = [];
+
+  if (Array.isArray(query)) {
+    queries.push(query);
+  } else {
+    mapValuesDeep(flatten(query), (value, key, obj) => {
+      if (key === '_id') {
+        ids.push(value);
+      } else if (key.includes('$in')) {
+        const parent = key.split('$in')[0].replace('.', '');
+        if (parent === '_id') {
+          ids.push(value);
+        } else if (Array.isArray(value)) {
+          value.map(query => [parent, '==', value]);
+          queries.push(value);
+        } else {
+          queries.push([parent, '==', value]);
+        }
+      }
+    });
+  }
+
+  return { queries, ids };
+};
+
+module.exports =  { convertQuery, mapValuesDeep, addOptionsToRef, addWhereToRef }
